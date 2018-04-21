579edf733bbf9eff8f0e9501e271c28ad84279dc
Mgicman <38139787+Mgicman@users.noreply.github.com>
2018-04-21 19:30:22 +0430

    Power

--- Power.py
+++ Power.py
@@ -11,7 +11,6 @@ class password:
         text = open('password list.text','w')    #for enter password in notpad
         pas = []                           #for input password
         has = []                            # for input hash's password
-        year = []
         try:
             count = int(raw_input('how many do you have information  about target(only enter number) ? '))  # enter how do you have information
         except ValueError:
@@ -27,13 +26,10 @@ class password:
             hassha224 = hashlib.sha224(b'%s' % (b)).hexdigest() # Convert password to hash(sha224)
             has.append(hassha224)                    #full list has
             has.append(hasmd5)
-        print(80 * '-')
+        
         print('this is your pas ---> {}' . format(pas))
-        print(80 * '-')
         print("hash's you'r password ----> {}" . format(has))
-        print(80 * '-')
-	print('')
-        print("warning : if you think the target doesnt have a strong password , don't enter (y) . because it will result in a longer password")
+
         mypas = raw_input("i change you'r password and add to you'r password (Y/N) ? ")
 
         if mypas == 'y' or mypas == 'Y':
@@ -48,68 +44,17 @@ class password:
                 end = end.replace('s','$')    # change s to $ and a to @
                 end = end.replace('i','!')   # change i to !
                 pasnew.append(end)
-            print(80 * '-')
+
             print("this is change you'r password -----> {} " . format(pasnew))
-            print(80 * '-')
             pasnew = pasnew+pasa
             pas = pasnew + pas           #add edite you'r password whith orginal password
         elif mypas == 'n' or mypas == 'N':
             pass
         else:
             print('you should only enter N or Y')
+                    
 
         ted = len(pas)
-	print('')
-        questionyear = raw_input("if you don't know the target date of birth . do you want me to find it for you (Y/N) ? ")
-        if questionyear == 'y' or questionyear == 'Y':
-            try:
-                print("hint : it will calculate in a range between the first year and second year , then it will add the reswt to the password list")
-                year1 = int(raw_input('enter first year : '))
-                year2 = int(raw_input('enter secound year : '))
-            except ValueError:
-                print('you should enter year')
-                sys.exit()
-
-            year1 -= 1		
-            while(year1 < year2):
-                year1 += 1
-                year.append(year1)
-            print(80 * '-')
-            print('this is years ----> {}' . format(year))
-            print(80 * '-')
-
-            count_year = len(year)
-
-            for pas_year1 in range(0,count_year):
-                for pas2_year in range(0,ted):
-                    text.write("{}{}\n". format(pas[pas2_year],year[pas_year1]))
-                    text.write("{}{}\n". format(year[pas_year1],pas[pas2_year]))
-                    
-            for pas_year2 in range(0,count_year):
-                for pas3_year in range(0,ted):
-                    for pas4_year in range(0,ted):
-                        text.write("{}{}{}\n" . format(pas[pas3_year] , pas[pas4_year] , year[pas_year2]))
-                        text.write("{}{}{}\n" . format(year[pas_year2] , pas[pas4_year] , pas[pas3_year]))
-                        text.write("{}{}{}\n" . format(pas[pas3_year] , year[pas_year2] , pas[pas4_year]))
-
-            for pas_year3 in range(0,count_year):
-                for pas5_year in range(0,ted):
-                    for pas6_year in range(0,ted):
-                        for pas7_year in range(0,ted):
-                            text.write("{}{}{}{}\n" . format(pas[pas5_year] , pas[pas6_year] , pas[pas7_year] , year[pas_year3]))
-                            text.write("{}{}{}{}\n" . format(pas[pas5_year] , pas[pas6_year] , year[pas_year3] , pas[pas7_year]))
-                            text.write("{}{}{}{}\n" . format(pas[pas5_year] , year[pas_year3] , pas[pas6_year] , pas[pas7_year]))
-                            text.write("{}{}{}{}\n" . format(year[pas_year3] , pas[pas5_year] , pas[pas6_year] , pas[pas7_year]))
-
-        elif question_year == 'n' or question_year == 'N':
-                pass
-        else:
-            print('you should enter Y or N')
-            sys.exit()
-
-
-
-        
         ted2 = len(has)
                                                   # make password list
         for d in range(0,ted):
@@ -149,7 +94,6 @@ class password:
 
 
         text.close()
-    
 
 
 os.system('clear')
@@ -172,28 +116,9 @@ print("""
                                                   
                 
                 """)
-try:
-    if sys.argv[1] == '-h' or sys.argv[1] == 'H':
-        print("""                  -r ----> run script  
-                  -v ----> Version of the program""")
-        sys.exit()
-
-    elif sys.argv[1] == '-r' or sys.argv[1] == 'R':
-        me=password()
-        me.pas1()
-
-    elif sys.argv[1] == '-v':
-        print('verssion 1.3')
-    else:
-        print("""              -h ----> Help 
-              -r ----> run script
-              -v ----> Version of the program  """)
-        
-except IndexError:
-    print("""              -h ----> Help 
-              -r ----> run script
-              -v ----> Version of the program  """)
 
 
+me=password()
+me.pas1()
 
 
